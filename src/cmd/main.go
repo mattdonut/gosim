f9b65b2416c9de0c306b3ec432eaf28f4b1ebaae
mattdonut <matt.donut.brunner@gmail.com>
2011-04-02 16:22:03 -0700

    Removed Print from poly, as well as dependance on fmt package

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -10,13 +10,10 @@ import (
 
 func main() {
 	fmt.Printf("Hello, world, again, and again!\n")
-	//chain := poly.NewRandom(8)
+
 	blank := poly.New(64)
 	blank.RandomLoc(vector.NewD3(0,0,0))
-	//blank.Loc[0] = vector.NewD3(0,0,0)
-	//blank.Loc[1] = vector.NewD3(1,0,0)
-	//blank.Loc[2] = vector.NewD3(2,0,0)
-	//blank.Loc[3] = vector.NewD3(3,.1,0)
+
 	fmt.Println(blank.ToJSON())
 	solver := new(solver.RK4)
 	solver.Init(blank)
--- src/pkg/poly/poly.go
+++ src/pkg/poly/poly.go
@@ -2,12 +2,11 @@ package poly
 
 import (
 	"vector"
-	"fmt"
 )
 
 type Chain struct {
 	length int
-	Loc, Vel, Acc []*vector.D3
+	Loc, Vel []*vector.D3
 }
 
 func New(length int) *Chain {
@@ -15,11 +14,9 @@ func New(length int) *Chain {
 	chain.length = length
 	chain.Loc = make([]*vector.D3,length)
 	chain.Vel = make([]*vector.D3,length)
-	chain.Acc = make([]*vector.D3,length)
 	for index ,_ := range chain.Loc {
 		chain.Loc[index] = vector.NewD3(0,0,0)
 		chain.Vel[index] = vector.NewD3(0,0,0)
-		chain.Acc[index] = vector.NewD3(0,0,0)
 	}
 	return chain
 }
@@ -29,13 +26,11 @@ func NewRandom(length int) *Chain {
 	chain.length = length
 	chain.Loc = make([]*vector.D3,length)
 	chain.Vel = make([]*vector.D3,length)
-	chain.Acc = make([]*vector.D3,length)
 	last := vector.NewD3(0,0,0)
 	for index ,_ := range chain.Loc {
 		chain.Loc[index] = last.Add(vector.RandomD3().Normalize())
 		last = chain.Loc[index]
 		chain.Vel[index] = vector.RandomD3().Normalize()
-		chain.Acc[index] = vector.RandomD3().Normalize()
 	}
 	return chain
 }
@@ -55,30 +50,8 @@ func (c *Chain) RandomVel() *Chain {
 	return c
 }
 
-func (c *Chain) RandomAcc() *Chain {
-	for index ,_ := range c.Acc {
-		c.Acc[index] = vector.RandomD3().Normalize()
-	}
-	return c
-}
-
 func (c *Chain) GetLength() int {
 	return c.length
-}
-
-func (c *Chain) Print() {
-	fmt.Printf("Locitions:\n")
-	for _ , value := range c.Loc {
-		fmt.Printf(value.String()+"\n")
-	}
-	fmt.Printf("Velocities:\n")
-	for _ , value := range c.Vel {
-		fmt.Printf(value.String()+"\n")
-	}
-//	fmt.Printf("Forces:\n")
-//	for _ , value := range c.Acc {
-//		fmt.Printf(value.String()+"\n")
-//	}
 }
 
 func (c *Chain) ToJSON() string {
